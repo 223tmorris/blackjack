d4db4ba96e80988fa28b93788063e4d96547b53c
223tmorris <84191365+223tmorris@users.noreply.github.com>
2022-11-28 07:46:28 -0500

    Add files via upload

---
+++ blackjack.js
@@ -0,0 +1,33 @@
+let rCount = 0;
+let tCount = 0;
+let decks = 8;
+let total = decks * 52;
+let remaining = total;
+
+function subtract(){
+	rCount--
+	document.getElementById("rCount").innerHTML = rCount;
+	
+	remaining--;
+	tCount = Math.trunc(rCount/(remaining/52));
+	document.getElementById("tCount").innerHTML = tCount;
+	console.log(remaining, Math.round(remaining/52), tCount);
+}
+
+function zero(){
+	remaining--;
+	let div = remaining/52;
+	tCount = Math.trunc(rCount/(remaining/52));
+	document.getElementById("tCount").innerHTML = tCount;
+	console.log(remaining, Math.round(remaining/52), tCount);
+}
+
+function add(){
+	rCount++;
+	document.getElementById("rCount").innerHTML = rCount;
+	
+	remaining--;
+	tCount = Math.trunc(rCount/Math.round(remaining/52));
+	document.getElementById("tCount").innerHTML = tCount;
+	console.log(remaining, Math.round(remaining/52), tCount);
+}
